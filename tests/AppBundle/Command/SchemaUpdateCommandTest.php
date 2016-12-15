89e84addd6483f308e5ae527eeda00efcb4f131e
gustawdaniel <gustaw.daniel@gmail.com>
2016-12-16 00:31:59 +0100

    Travis

--- tests/AppBundle/Command/SchemaUpdateCommandTest.php
+++ tests/AppBundle/Command/SchemaUpdateCommandTest.php
@@ -9,6 +9,8 @@
 namespace Tests\AppBundle\Command;
 
 use AppBundle\Command\SchemaUpdateCommand;
+use Doctrine\ORM\Tools\Console\Command\SchemaTool\CreateCommand;
+use Doctrine\ORM\Tools\Console\Command\SchemaTool\DropCommand;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Console\Tester\CommandTester;
@@ -32,40 +34,40 @@ class SchemaUpdateCommandTest extends KernelTestCase
      */
     private $commandTester;
 
+
+    private function resetDatabase()
+    {
+        $app = new Application(self::$kernel);
+        $app->add(new DropCommand());
+        $app->add(new CreateCommand());
+        (new CommandTester($app->find('doctrine:database:drop')))->execute(['--force' => true]);
+        (new CommandTester($app->find('doctrine:database:create')))->execute([]);
+    }
+
     /**
      * {@inheritDoc}
      */
     protected function setUp()
     {
         self::bootKernel();
-        $this->conn = static::$kernel->getContainer()->get('doctrine')->getConnection();
+        $this->resetDatabase();
         $app = new Application(self::$kernel);
         $app->add(new SchemaUpdateCommand());
         $this->command = $app->find('app:schema:update');
         $this->commandTester = new CommandTester($this->command);
+        $this->conn = static::$kernel->getContainer()->get('doctrine')->getConnection();
     }
 
     public function testExecute()
     {
-        $this->commandTester->execute(array(
-            'command'  => $this->command->getName(),
-        ));
+        $this->commandTester->execute([]);
         $this->assertContains('Please run the operation by passing one - or both - of the following options:', $this->commandTester->getDisplay());
 
-        $this->commandTester->execute(array(
-            'command'  => $this->command->getName(),
-            'N' => 1, '--force' => true, '--dump-sql' => true
-        ));
+        $this->commandTester->execute(['N' => 1, '--force' => true, '--dump-sql' => true]);
         $this->assertContains('ALTER TABLE', $this->commandTester->getDisplay());
 
-
-        $this->commandTester->execute(array(
-            'command'  => $this->command->getName(),
-            'N' => 2, '--force' => true
-        ));
-
+        $this->commandTester->execute(['N' => 2, '--force' => true]);
         $this->assertContains('Database schema updated successfully!', $this->commandTester->getDisplay());
-
     }
 
     protected function tearDown()
