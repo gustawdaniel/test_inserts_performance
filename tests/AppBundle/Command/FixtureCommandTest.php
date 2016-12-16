914b926575f9400344daf08235a05c78ec5d7b15
gustawdaniel <gustaw.daniel@gmail.com>
2016-12-16 05:41:18 +0100

    Fixtures major and minor work ok!

--- tests/AppBundle/Command/FixtureCommandTest.php
+++ tests/AppBundle/Command/FixtureCommandTest.php
@@ -19,5 +19,23 @@ class FixtureCommandTest extends BaseTestCase
         $this->doTest(['table' => 'minor']);
         $res = $this->conn->fetchColumn('SELECT COUNT(*) FROM minor_1');
         $this->assertEquals($res,static::$kernel->getContainer()->getParameter('L'));
+
+        $res = $this->conn->fetchColumn('select count(*) from main_1');
+        $this->assertEquals($res,0);
+
+        $this->doTest(['table'=>'major', 'N'=>10, 'L'=>20, 'K'=>10]);
+        $res = $this->conn->fetchColumn('select count(*) from main_1');
+        $this->assertEquals($res,10);
+
+        $this->doTest(['table'=>'major', 'N'=>10, 'L'=>20, 'K'=>10, '--append'=>true]);
+        $res = $this->conn->fetchColumn('select count(*) from main_1');
+        $this->assertEquals($res,20);
+
+        $res = $this->conn->fetchColumn('select count(*) from main_2');
+        $this->assertEquals($res,0);
+
+        $this->doTest(['table'=>'major', 'N'=>10, 'L'=>50, 'K'=>5, '--append'=>true, 'main'=>2]);
+        $res = $this->conn->fetchColumn('select count(*) from main_2');
+        $this->assertEquals($res,5);
     }
 }
