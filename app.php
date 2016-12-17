e1afdf25abc54c7cac6b00cb7f9379c94b23461d
gustawdaniel <gustaw.daniel@gmail.com>
2016-12-17 21:22:57 +0100

    New approach

--- app.php
+++ app.php
@@ -1,7 +1,11 @@
 <?php
 
 require_once "vendor/autoload.php";
+require_once 'model/SchemaGenerator.php';
 use Doctrine\DBAL\DriverManager;
+use Model\SchemaGenerator;
+use Doctrine\DBAL\Schema\Comparator;
+use Symfony\Component\Console\Helper\ProgressBar;
 
 $connectionParams = array(
     'dbname' => 'training',
@@ -13,9 +17,32 @@ $connectionParams = array(
 $conn = DriverManager::getConnection($connectionParams);
 
 
-$sql = "SELECT a FROM o";
-$stmt = $conn->query($sql); // Simple, but has several drawbacks
+//$sql = "SELECT a FROM o";
+//$stmt = $conn->query($sql); // Simple, but has several drawbacks
+//
+//while ($row = $stmt->fetch()) {
+//    echo $row['a'];
+//}
 
-while ($row = $stmt->fetch()) {
-    echo $row['a'];
+
+$schema = (new SchemaGenerator(10))->generate();
+$queries = (new Comparator())->compare($conn->getSchemaManager()->createSchema(), $schema)->toSql($conn->getDatabasePlatform());
+
+
+//        $progress = new ProgressBar($output, count($queries));
+//        $progress->setFormat('very_verbose');
+//        $progress->start();
+    foreach($queries as $key => $query) {
+        $conn->prepare($query)->execute();
+//        $progress->advance();
+        progressBar($key,count($queries));
+    }
+//        $progress->finish();
+
+
+function progressBar($done, $total) {
+    $perc = floor(($done / $total) * 100);
+    $left = 100 - $perc;
+    $write = sprintf("\033[0G\033[2K[%'={$perc}s>%-{$left}s] - $perc%% - $done/$total", "", "");
+    fwrite(STDERR, $write);
 }
--- src/AppBundle/Command/SchemaUpdateCommand.php
+++ src/AppBundle/Command/SchemaUpdateCommand.php
@@ -23,32 +23,31 @@ class SchemaUpdateCommand extends Base
     private $n,$q;
 
 
-    public function configure()
-    {
-        $this
-            ->setName('app:schema:update')
-            ->addOption('dump-sql', null, InputOption::VALUE_NONE,'Dumps the generated SQL statements to the screen (does not execute them).')
-            ->addOption('force', 'f', InputOption::VALUE_NONE, 'Causes the generated SQL statements to be physically executed against your database.')
-            ->addArgument('N', InputArgument::OPTIONAL, 'Number of minor tables');
-    }
+//    public function configure()
+//    {
+//        $this
+//            ->setName('app:schema:update')
+//            ->addOption('dump-sql', null, InputOption::VALUE_NONE,'Dumps the generated SQL statements to the screen (does not execute them).')
+//            ->addOption('force', 'f', InputOption::VALUE_NONE, 'Causes the generated SQL statements to be physically executed against your database.')
+//            ->addArgument('N', InputArgument::OPTIONAL, 'Number of minor tables');
+//    }
 
-    protected function interact(InputInterface $input, OutputInterface $output)
-    {
-        $this->n = $input->getArgument('N') ?: $this->getContainer()->getParameter('N');
-        $this->q = $input->getOption('quiet');
-
-        if (!$input->getOption('force') && !$input->getOption('dump-sql') ) {
-            $output->writeln('Please run the operation by passing one - or both - of the following options:');
-            $output->writeln(sprintf('    <info>%s --force</info> to execute the command', $this->getName()));
-            $output->writeln(sprintf('    <info>%s --dump-sql</info> to dump the SQL statements to the screen', $this->getName()));
-            return 0;
-        }
-    }
+//    protected function interact(InputInterface $input, OutputInterface $output)
+//    {
+//        $this->n = $input->getArgument('N') ?: $this->getContainer()->getParameter('N');
+//        $this->q = $input->getOption('quiet');
+//
+//        if (!$input->getOption('force') && !$input->getOption('dump-sql') ) {
+//            $output->writeln('Please run the operation by passing one - or both - of the following options:');
+//            $output->writeln(sprintf('    <info>%s --force</info> to execute the command', $this->getName()));
+//            $output->writeln(sprintf('    <info>%s --dump-sql</info> to dump the SQL statements to the screen', $this->getName()));
+//            return 0;
+//        }
+//    }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $schema = (new SchemaGenerator($this->n))->generate();
-        $queries = (new Comparator())->compare($this->conn->getSchemaManager()->createSchema(), $schema)->toSql($this->conn->getDatabasePlatform());
+
 
         if (!$queries) { $output->writeln('Nothing to update - your database is sync with given state.'); $this->conn->close(); return 0; }
         if ($input->getOption('dump-sql')) { $output->writeln(implode(';' . PHP_EOL, $queries) . ';');}
@@ -56,24 +55,5 @@ class SchemaUpdateCommand extends Base
         $this->conn->close();
     }
 
-    private function rebuildDatabase(OutputInterface $output, $queries)
-    {
-        if(!$this->q) {
-            $output->writeln('Updating database schema...');
 
-            $progress = new ProgressBar($output, count($queries));
-            $progress->setFormat('very_verbose');
-            $progress->start();
-        }
-        foreach($queries as $key => $query) {
-            $this->conn->prepare($query)->execute();
-            $this->q?:$progress->advance();
-        }
-        if(!$this->q) {
-            $progress->finish();
-
-            $pluralization = (1 === count($queries)) ? 'query was' : 'queries were';
-            $output->writeln(sprintf("\n".'Database schema updated successfully! "<info>%s</info>" %s executed', count($queries), $pluralization));
-        }
-    }
 }
