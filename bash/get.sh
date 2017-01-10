837a988930bdd6d64f4a0016ae8cc72b38086ce6
gustawdaniel <gustaw.daniel@gmail.com>
2017-01-10 21:53:14 +0100

    Bath without first line

--- bash/get.sh
+++ bash/get.sh
@@ -2,19 +2,38 @@
 
 cd $(dirname ${BASH_SOURCE[0]});
 
+. lib/parse_yaml.sh
+eval $(parse_yaml ../config/parameters.yml "config_")
 
-curl -X POST https://api.dropboxapi.com/2/files/list_folder \
-    --header "Authorization: Bearer 8CqvxAXxLjIAAAAAAAAXadmk8MuiotLEkiHlTqyw0SbKT3QCe4lwxXvwVh02iv6r" \
-    --header "Content-Type: application/json" \
-    --data "{\"path\": \"\",\"recursive\": false,\"include_media_info\": false,\"include_deleted\": false,\"include_has_explicit_shared_members\": false}" \
-     | jq  '.entries[] | .path_lower' | tee build/ext_files_list.txt
+#curl -X POST https://api.dropboxapi.com/2/files/list_folder \
+#    --header "Authorization: Bearer 8CqvxAXxLjIAAAAAAAAXadmk8MuiotLEkiHlTqyw0SbKT3QCe4lwxXvwVh02iv6r" \
+#    --header "Content-Type: application/json" \
+#    --data "{\"path\": \"\",\"recursive\": false,\"include_media_info\": false,\"include_deleted\": false,\"include_has_explicit_shared_members\": false}" \
+#     | jq  '.entries[] | .path_lower' | tr -d "\"" > build/ext_files_list.txt
 
-
-
+mysql -u root $config_parameters_dbname -e \
+    "TRUNCATE log; DELETE FROM machine";
 
 while read p; do
   echo $p;
-  curl -X POST https://content.dropboxapi.com/2/files/download \
-    --header 'Authorization: Bearer 8CqvxAXxLjIAAAAAAAAXadmk8MuiotLEkiHlTqyw0SbKT3QCe4lwxXvwVh02iv6r' \
-    --header "Dropbox-API-Arg: {\"path\":$p}";
+#  curl -X POST https://content.dropboxapi.com/2/files/download \
+#    --header 'Authorization: Bearer 8CqvxAXxLjIAAAAAAAAXadmk8MuiotLEkiHlTqyw0SbKT3QCe4lwxXvwVh02iv6r' \
+#    --header "Dropbox-API-Arg: {\"path\":\"$p\"}" > build$p;
+#
+
+    if [[ $p == "log_"* ]]
+    then
+      echo "LOG: $p !";
+      mysql -u root $config_parameters_dbname -e \
+      "LOAD DATA INFILE 'build/machine_fcb75356-d759-11e6-bbe0-c04a003d4415.txt' INTO TABLE log;";
+    fi
+
+
+    if [[ $p == "machine_"* ]]
+    then
+      echo "MACHINE: $p !";
+        mysql -u root $config_parameters_dbname -e \
+        "LOAD DATA INFILE 'build$p' INTO TABLE machine;";
+    fi
+
 done < build/ext_files_list.txt
--- bash/send.sh
+++ bash/send.sh
@@ -5,15 +5,9 @@ cd $(dirname ${BASH_SOURCE[0]});
 . lib/parse_yaml.sh
 eval $(parse_yaml ../config/parameters.yml "config_")
 
-#echo $config_parameters_token;
-#echo $config_parameters_guid;
-
-#mysql -uroot training -e "select n,l,k,t from log" -B > build/log.tsv
-#mysql -uroot training -e "select name,\`write\`,\`read\`,latency,cpu from machine" -B > build/machine.tsv
-
 for i in log machine
 do
-    mysql -uroot training -e "select * from $i" -B > build/$i.tsv
+    mysql -uroot training -e "select * from $i" -B | tail -n +2 > build/$i.tsv
 
     curl -X POST https://content.dropboxapi.com/2/files/upload \
     --header "Authorization: Bearer $config_parameters_token" \
